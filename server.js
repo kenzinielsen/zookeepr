f50e082bec5d4935c9ab42db35a6724e4d27cab5
kenzinielsen <kenzinielsen@kenzis-air.lan>
2021-09-28 15:32:43 -0600

    heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const {animals} = require('./data/animals.json')
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -45,16 +46,7 @@ function filterByQuery(query, animalsArray) {
     }
     res.json(results);
   });
-//app.get('/api/animals', (req, res) => {
-//    let results = animals;
-//    if (req,query) {
-//        results = filterByQuery(req.query, results);
-//    }
-//    res.json(results);
-//
-//    console.log(req.query)
-//    res.json(animals);
-//})
-app.listen(3001, () => {
-    console.log('API server now on port 3001')
-})
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
